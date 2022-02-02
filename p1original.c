c9f0203bebd9e1c91fbc7bc312136c365856d910
NISHANTHR4 <97089406+NISHANTHR4@users.noreply.github.com>
2022-02-02 04:56:25 +0000

    p1original

--- p1original.c
+++ p1original.c
@@ -1,2 +1,21 @@
-
+#include<stdio.h>
+void input(int *a,int *b)
+{
+printf("Enter any 2 numbers\n");
+scanf("%d%d",a,b);
+}
+void add(int a,int b,int *sum)
+{
+*sum= a+b;
+}
+void output(int a,int b,int sum)
+{
+printf("The sum of %d and %d is = %d\n",a,b,sum);
+}
+void main()
+{
+  int a,b,sum;
+input(&a,&b);
+add(a,b,&sum);
+output(a,b,sum);
 }
