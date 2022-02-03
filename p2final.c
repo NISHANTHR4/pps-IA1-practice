051b42f5a1a3a66e543aa50565fddcfdf096aaa5
NISHANTHR4 <97089406+NISHANTHR4@users.noreply.github.com>
2022-02-03 10:29:38 +0000

    p2debug

---
+++ p2final.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int input()
+{
+  int a;
+printf("Enter any three numbers to compare\n");
+scanf("%d",&a);
+return a;
+}
+int cmp(int a, int b, int c)
+{
+  int largest;
+if(a>=b&&a>=c)
+largest = a;
+if(a<=b && b>=c)
+largest = b;
+else
+largest = c;
+return largest;
+}
+void output(int num)
+{
+  int a,b,c,largest;
+printf("The largest number  is %d\n",num);
+}
+void main()
+{
+  int a,b,c,largest,num1,num2,num3,cmpr,num;
+num1=input();
+num2=input();
+num3=input();
+cmpr=cmp(num1,num2,num3);
+output(cmpr);
+}
